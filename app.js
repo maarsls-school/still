53b0151958a28fb36ef8912a1b6087200a33dfbb
myhpn <marcel.maffey@gmail.com>
2021-06-23 17:28:51 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, { debug: true, });
+
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+app.get("/", (req, res) => { res.redirect(`/${uuidv4()}`); });
+app.get("/:room", (req, res) => { res.render("room", { roomId: req.param.room }); });
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+    });
+});
+server.listen(3030);
